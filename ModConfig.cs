085d4c468e76b069f811a4622c0eb2a4244c30c3
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-11-17 18:23:37 +0100

    Added fish sprites folder initialization

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,7 +1,11 @@
 ﻿using BepInEx.Configuration;
+using BepInEx;
+using Cpp2IL.Core.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -11,6 +15,8 @@ namespace AmongUsFishing {
         public static ConfigEntry<KeyCode> FishingKey { get; private set; }
 
         public static void Init(ConfigFile config) {
+            InitFishSpritesFolder(Paths.ConfigPath);
+
             FishingKey = config.Bind(
                 "Input",
                 "FishingKey",
@@ -18,5 +24,24 @@ namespace AmongUsFishing {
                 "The key to use to start (and stop) fishing"
             );
         }
+
+        static void InitFishSpritesFolder(string path) {
+            var fishSpritesFolder = Path.Combine(path, $"{Plugin.GUID}-FishSprites");
+            if (Directory.Exists(fishSpritesFolder))
+                return;
+
+            Directory.CreateDirectory(fishSpritesFolder);
+            var assembly = Assembly.GetExecutingAssembly();
+
+            foreach (var fishSprite in assembly.GetManifestResourceNames()) {
+                var fileName = fishSprite.Split('.', 3)[2];
+
+                using Stream stream = assembly.GetManifestResourceStream(fishSprite);
+                using Stream file = File.OpenWrite(Path.Combine(fishSpritesFolder, fileName));
+
+                stream.CopyTo(file);
+            }
+        }
+
     }
 }
