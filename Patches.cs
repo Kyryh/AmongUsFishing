c3ca313e99d397493f18ebfbc236cde14adcc3c6
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-11-17 16:22:23 +0100

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+using static CatchFishMinigame;
+
+
+namespace AmongUsFishing {
+    internal static class Patches {
+        public static void Init() {
+            Harmony.CreateAndPatchAll(typeof(Patches), Plugin.GUID);
+        }
+
+        [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.Awake))]
+        [HarmonyPostfix]
+        static void InitMinigamePrefab(AmongUsClient __instance) {
+            if (Plugin.MinigamePrefab != null)
+                return;
+
+            Plugin.Log.LogInfo("Obtaining fishing minigame prefab");
+
+            var funglePrefab = __instance.ShipPrefabs[(int)MapNames.Fungle].LoadAsset<GameObject>().WaitForCompletion(); ;
+            var fishingTask = funglePrefab.GetComponent<ShipStatus>().GetAllTasks().First(t => t.TaskType == TaskTypes.CatchFish);
+            Plugin.MinigamePrefab = fishingTask.MinigamePrefab.transform.Find("CatchFishMinigame").GetComponent<CatchFishMinigame>();
+
+            Plugin.Log.LogInfo("Prefab obtained");
+        }
+
+        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+        [HarmonyPostfix]
+        static void CheckInput() {
+
+            if (Input.GetKeyDown(KeyCode.P)) {
+                Plugin.ToggleFishingMinigame();
+            }
+        }
+
+        [HarmonyPatch(typeof(_CoUpdateProgress_d__42), nameof(_CoUpdateProgress_d__42.MoveNext))]
+        [HarmonyPrefix]
+        static bool CoUpdateProgressPatch(_CoUpdateProgress_d__42 __instance, ref bool __result) {
+            // This updates the task's progress and we definitely don't want that
+            // since this isn't a real task, so we skip it completely
+            // Also we start the CoBeginFishing coroutine since
+            // the original method did it and we skipped it
+
+            __instance.__4__this.StartCoroutine("CoBeginFishing");
+            __result = false; 
+
+            // Change the fish sprite just for fun
+            __instance.__4__this.RandomizeFishSprite();
+
+            return false;
+        }
+
+
+        [HarmonyPatch(typeof(_CoAnimateCaughtFish_d__41), nameof(_CoAnimateCaughtFish_d__41.MoveNext))]
+        [HarmonyPrefix]
+        static bool CoAnimateCaughtFishPatch(_CoAnimateCaughtFish_d__41 __instance, ref bool __result) {
+            // This just does some animation stuff or something idk
+            // The last step calls CatchFishMinigame.SetCaughtFish and increments
+            // CatchFishMinigame.numCaughtFish, so we skip only the last step
+
+            if (__instance.__1__state == 2) {
+                __result = false;
+                return false;
+            }
+
+            return true;
+        }
+
+        static void RandomizeFishSprite(this CatchFishMinigame minigame) {
+            minigame.ChangeFishSprite((byte)Random.Range(0, minigame.fishCounterSprites.Length));
+        }
+        static void ChangeFishSprite(this CatchFishMinigame minigame, byte spriteIndex) {
+            minigame.MyNormTask.Data[1] = spriteIndex;
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using System;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace AmongUsFishing;
+
+[BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    public const string GUID = $"Kyryh.{MyPluginInfo.PLUGIN_GUID}";
+
+    internal static new ManualLogSource Log;
+
+    static CatchFishMinigame minigamePrefab;
+
+    static CatchFishMinigame minigameInstance;
+    static NormalPlayerTask minigameTask;
+    public static CatchFishMinigame MinigamePrefab {
+        get {
+            return minigamePrefab;
+        }
+        set {
+            if (minigamePrefab != null)
+                throw new InvalidOperationException();
+            minigamePrefab = value;
+        } 
+    }
+    public override void Load() {
+        // Plugin startup logic
+        Log = base.Log;
+
+        Patches.Init();
+
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public static void ToggleFishingMinigame() {
+        if (minigameInstance == null) {
+            minigameInstance = UObject.Instantiate(MinigamePrefab, Camera.main.transform, false);
+            minigameInstance.transform.Find("FishCounters").gameObject.SetActive(false);
+            minigameInstance.transform.localPosition = new Vector3(0.0f, 0.0f, -50f);
+            minigameTask = minigameInstance.gameObject.AddComponent<NormalPlayerTask>();
+            minigameTask.Data = new byte[4];
+        }
+
+        if (!minigameInstance.isActiveAndEnabled) {
+            minigameInstance.gameObject.SetActive(true);
+            minigameInstance.Begin(minigameTask);
+        } else {
+            minigameInstance.Close();
+        }
+    }
+}
